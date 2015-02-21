a6128184d6ad0b0299618ee5235904a1e3219064
Zhenek1 <mistz@mail.ru>
2015-02-22 02:29:58 +0300

    Commit + Push

--- src/Clone.java
+++ src/Clone.java
@@ -11,6 +11,9 @@ import org.eclipse.jgit.api.errors.JGitInternalException;
 import org.eclipse.jgit.api.errors.TransportException;
 import org.eclipse.jgit.lib.ProgressMonitor;
 import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.transport.PushResult;
+import org.eclipse.jgit.transport.RefSpec;
+import org.eclipse.jgit.transport.RemoteRefUpdate;
 import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
 import org.tukaani.xz.XZInputStream;
 
@@ -27,6 +30,8 @@ public class Clone {
     public static final int COMMIT_TYPE_TAG = 1;
     public static final int COMMIT_TYPE_TEMP = 2;
     public static final int COMMIT_TYPE_REMOTE = 3;
+
+    private static StringBuffer resultMsg = new StringBuffer();
 
    // static Git g;
     static Reposit r = new Reposit();
@@ -45,6 +50,8 @@ public class Clone {
        // Clone.checkout(r, arr[2]);
         r.git = getGit(r);
         boolean b = pullRepo(r);
+        boolean c = commitChanges(r, "test commit message", true);
+        boolean d = pushRepo(r, true);
     }
 
     public static int getCommitType(String[] splits) {
@@ -184,6 +191,124 @@ public class Clone {
 
         return true;
     }
+
+    public static boolean commitChanges(Reposit reposit, String commitMessage, boolean stageAll) {
+        Git git;
+        try {
+            git = getGit(reposit);
+        } catch (Exception e) {
+            return false;
+        }
+        AddCommand add_all = git.add().addFilepattern("."); // add all files to stage
+        CommitCommand cc = git.commit().setAll(stageAll).setMessage(commitMessage);
+
+        try {
+            add_all.call();
+            cc.call();
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+    public static boolean pushRepo(Reposit reposit, boolean pushAll) {
+        Git git;
+        try {
+            git = getGit(reposit);
+        } catch (Exception e) {
+            return false;
+        }
+        PushCommand pushCommand = git.push().setPushTags()
+                .setProgressMonitor(new RepoCloneMonitor())
+                .setRemote(reposit.getRemoteURL());
+        if (pushAll) {
+            pushCommand.setPushAll();
+        } else {
+            RefSpec spec = new RefSpec(reposit.getBranchName());
+            pushCommand.setRefSpecs(spec);
+        }
+
+        String username = reposit.getUsername();
+        String password = reposit.getPassword();
+        if (username != null && password != null && !username.equals("")
+                && !password.equals("")) {
+            UsernamePasswordCredentialsProvider auth = new UsernamePasswordCredentialsProvider(
+                    username, password);
+            pushCommand.setCredentialsProvider(auth);
+        }
+
+        try {
+            Iterable<PushResult> result = pushCommand.call();
+            for (PushResult r : result) {
+                Collection<RemoteRefUpdate> updates = r.getRemoteUpdates();
+                for (RemoteRefUpdate update : updates) {
+                    parseRemoteRefUpdate(update);
+                }
+            }
+        } catch (Exception e) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private static void parseRemoteRefUpdate(RemoteRefUpdate update) {
+        String msg = null;
+        switch (update.getStatus()) {
+            case AWAITING_REPORT:
+                msg = String
+                        .format("[%s] Push process is awaiting update report from remote repository.\n",
+                                update.getRemoteName());
+                break;
+            case NON_EXISTING:
+                msg = String.format("[%s] Remote ref didn't exist.\n",
+                        update.getRemoteName());
+                break;
+            case NOT_ATTEMPTED:
+                msg = String
+                        .format("[%s] Push process hasn't yet attempted to update this ref.\n",
+                                update.getRemoteName());
+                break;
+            case OK:
+                msg = String.format("[%s] Success push to remote ref.\n",
+                        update.getRemoteName());
+                break;
+            case REJECTED_NODELETE:
+                msg = String
+                        .format("[%s] Remote ref update was rejected,"
+                                        + " because remote side doesn't support/allow deleting refs.\n",
+                                update.getRemoteName());
+                break;
+            case REJECTED_NONFASTFORWARD:
+                msg = String.format("[%s] Remote ref update was rejected,"
+                                + " as it would cause non fast-forward update.\n",
+                        update.getRemoteName());
+            case REJECTED_OTHER_REASON:
+                String reason = update.getMessage();
+                if (reason == null || reason.isEmpty()) {
+                    msg = String.format(
+                            "[%s] Remote ref update was rejected.\n",
+                            update.getRemoteName());
+                } else {
+                    msg = String
+                            .format("[%s] Remote ref update was rejected, because %s.\n",
+                                    update.getRemoteName(), reason);
+                }
+                break;
+            case REJECTED_REMOTE_CHANGED:
+                msg = String
+                        .format("[%s] Remote ref update was rejected,"
+                                        + " because old object id on remote "
+                                        + "repository wasn't the same as defined expected old object.\n",
+                                update.getRemoteName());
+                break;
+            case UP_TO_DATE:
+                msg = String.format("[%s] remote ref is up to date\n",
+                        update.getRemoteName());
+                break;
+        }
+        resultMsg.append(msg);
+    }
+
     // Uses cloneCommand to clone repository
     public static boolean cloneRepo(Reposit reposit) {
         System.err.println("Clone procedure begin...");
@@ -342,6 +467,15 @@ public class Clone {
         }
 
         public Git git;
+
+        public String getBranchName(){
+            try {
+                return git.getRepository().getFullBranch();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
 
         public String getRemoteURL() {
             return "https://abf.io/creepycheese/pvpgn.git";
