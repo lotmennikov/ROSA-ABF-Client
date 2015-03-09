4b0a1cb48cadd531e7d72ce1e9f7fd1092082e1e
lotmennikov <lotmennikov@gmail.com>
2015-03-10 00:51:15 +0300

    Merge branch 'master' of https://github.com/lotmennikov/ROSA-ABF-Client
    Conflicts:
    	.idea/modules.xml
    	.idea/workspace.xml
    
    Поправки интерфейса
    
    Merge remote-tracking branch
    'origin/jgit_commands'
    
    Conflicts:
    	.idea/modules.xml
    	.idea/workspace.xml

--- app/src/main/java/hse/zhizh/abfclient/common/Settings.java
+++ app/src/main/java/hse/zhizh/abfclient/common/Settings.java
@@ -1,6 +1,7 @@
 package hse.zhizh.abfclient.common;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -10,6 +11,7 @@ import java.util.List;
 import hse.zhizh.abfclient.Model.Project;
 import hse.zhizh.abfclient.Model.Repository;
 import hse.zhizh.abfclient.Model.User;
+import hse.zhizh.abfclient.R;
 
 /**
  * Created by E-Lev on 02.02.2015.
@@ -26,45 +28,47 @@ public class Settings {
     public static String repo_password = "fab688";
 
     // user projects
-    public static List<Project> projects;
+//    public static List<Project> projects;
 
     // selected project
     public static Project currentProject;
 
     // list of active repositories
-    public static List<Repository> repositoryList;
-
+//    public static List<Repository> repositoryList;
     // current repository (temp)
-    public static Repository currentRepository;
-
+//    public static Repository currentRepository;
     // current user
-    public static User currentUser;
+//    public static User currentUser;
 
     // getApplicationContext()
     public static Context appContext;
 
     static {
-        repositoryList = new ArrayList<Repository>();
-        currentUser = null;
-        currentRepository = null;
-        projects = initProjects();
-        currentProject = projects.get(0);
+//        repositoryList = new ArrayList<Repository>();
+//        currentUser = null;
+//        currentRepository = null;
+        currentProject = null;
+//        projects = initProjects();
+//        currentProject = projects.get(0);
     }
-
-    // temporary initialization
-    // TODO replace by loading from database
-    static List<Project> initProjects() {
-        List<Project> projects = new ArrayList<>();
-        Project testProject = new Project(1, "0ad", "lotmen/0ad", "lotmen/0ad.git", "descr", 1);
-//        testProject.initRepo();
-        projects.add(testProject);
-
-        // some other projects
-
-
-        return projects;
-    }
-
 
     public static DateFormat commitDate = new SimpleDateFormat("dd.MM.yyyy");
+
+// заглушечный тупейший менеджер паролей
+
+    public static String getFromTempPasswordHolder(Context context, String username) {
+       // SharedPreferences prefs = context.getSharedPreferences("")
+        SharedPreferences shp = context.getSharedPreferences(context.getString(R.string.app_preferences_file), Context.MODE_PRIVATE);
+        String password = shp.getString("password_"+username, "");
+        return password;
+    }
+
+    public static void setTempPasswordHolder(Context context, String username, String password) {
+        SharedPreferences shp = context.getSharedPreferences(context.getString(R.string.app_preferences_file), Context.MODE_PRIVATE);
+        shp.edit()
+                .putString("password_"+username, password)
+                .apply();
+    }
+
+
 }
