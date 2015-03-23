ad2d1ebc37f9dba59b26885837c56261e9ad7870
lotmennikov <lotmennikov@gmail.com>
2015-03-23 10:18:16 +0300

    TrustAllHosts

--- app/src/main/java/hse/zhizh/abfclient/Session/SessionImpl.java
+++ app/src/main/java/hse/zhizh/abfclient/Session/SessionImpl.java
@@ -10,8 +10,16 @@ import java.net.MalformedURLException;
 import java.net.PasswordAuthentication;
 import java.net.URL;
 import java.net.URLConnection;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 
+import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 import hse.zhizh.abfclient.common.Settings;
 
@@ -46,6 +54,43 @@ public class SessionImpl implements Session {
 
     }
 
+    // always verify the host - dont check for certificate
+    final static HostnameVerifier DO_NOT_VERIFY = new HostnameVerifier() {
+        public boolean verify(String hostname, SSLSession session) {
+            return true;
+        }
+    };
+
+    /**
+     * Trust every server - dont check for any certificate
+     */
+    private static void trustAllHosts() {
+        // Create a trust manager that does not validate certificate chains
+        TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager() {
+            public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                return new java.security.cert.X509Certificate[]{};
+            }
+
+            public void checkClientTrusted(X509Certificate[] chain,
+                                           String authType) throws CertificateException {
+            }
+
+            public void checkServerTrusted(X509Certificate[] chain,
+                                           String authType) throws CertificateException {
+            }
+        }};
+
+        // Install the all-trusting trust manager
+        try {
+            SSLContext sc = SSLContext.getInstance("TLS");
+            sc.init(null, trustAllCerts, new java.security.SecureRandom());
+            HttpsURLConnection
+                    .setDefaultSSLSocketFactory(sc.getSocketFactory());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     //Действие аутентефикации(задает имя пользователя и пароль по умолчанию для соединений)
     public static void authenticate(HttpsURLConnection con){
     /*    Authenticator.setDefault(new Authenticator() {
@@ -64,6 +109,7 @@ public class SessionImpl implements Session {
     public HttpsURLConnection createConnection() throws Exception {
         String https_url = "https://abf.rosalinux.ru/api/v1/groups.json";
         URL url;
+        trustAllHosts();
         HttpsURLConnection con=null;
         try {
           /*  String encoding = Base64.encodeToString(new String(username + ":" + userpass).getBytes(), Base64.NO_WRAP);
